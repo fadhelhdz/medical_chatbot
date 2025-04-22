7c2b4afee25d3aacdd968b505bc840b2422ae367
fadhelhdz <hariz.dz97@gmail.com>
2025-04-22 13:07:10 +0900

    project structures done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_bot",
+    version="0.0.0",
+    author="fadhelhdz",
+    author_email="fadhelharizdz@gmail.com",
+    description="Medical AI Chatbot",
+    packages=find_packages(),
+    install_requires=[],
+)
